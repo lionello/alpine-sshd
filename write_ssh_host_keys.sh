58055cc8b82a28f0869da96083be14d0a2ed94f9
Lionello Lunesu <lio+git@lunesu.com>
2025-12-15 08:50:51 +0800

    fix: better log message in write_ssh_host_keys.sh

--- write_ssh_host_keys.sh
+++ write_ssh_host_keys.sh
@@ -2,12 +2,12 @@
 umask 077
 env -0 | while IFS='=' read -r -d '' n v; do
   if [[ $n == ssh_host_* ]]; then
-    n=/etc/ssh/${n/_pub/.pub} # replace _pub with .pub
+    f=/etc/ssh/${n/_pub/.pub} # replace _pub with .pub
   elif [[ $n == *_authorized_keys ]]; then
-    n=/home/${n/_authorized_keys/\/.ssh\/authorized_keys} # prefix is username
+    f=/home/${n/_authorized_keys/\/.ssh\/authorized_keys} # prefix is username
   else
     continue
   fi
-  echo Writing $n
-  printf '%s\n' "$v" >> $n
+  echo Writing \$$n to $f
+  printf '%s\n' "$v" >> $f
 done
